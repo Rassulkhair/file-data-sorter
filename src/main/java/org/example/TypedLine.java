2f7b8a41c606b21c8818a4b846da7ba79cd3fb40
Rassulkhair <rassulkhairshokanov@gmail.com>
2025-08-07 23:56:30 +0500

    .

---
+++ src/main/java/org/example/TypedLine.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class TypedLine {
+    public final DataType type;
+    public final String line;
+
+    public TypedLine(DataType type, String line) {
+        this.type = type;
+        this.line = line;
+    }
+}
